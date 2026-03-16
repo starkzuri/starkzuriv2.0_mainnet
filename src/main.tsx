c89469ab1818e5bb13ecccb56b61896fc362482c
felabs1 <felixawere01@gmail.com>
2026-03-17 01:56:21 +0300

    set email as primary login

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById("root")!).render(
     <PrivyProvider
       appId={privyAppId}
       config={{
-        loginMethods: ["google", "wallet"],
+        loginMethods: ["email", "wallet"],
         appearance: { theme: "dark" },
       }}
     >
